a2e07a1403db21c2a796b5ddd6d4043ecffe5e96
Fernando Cardona <fernando.cardona@the-lion.com>
2022-05-30 18:37:26 +0200

    Form starting and API Call

--- src/componets/SearchForm.js
+++ src/componets/SearchForm.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react'
-const searchForm = () => {
-    return ( 
-        
-            <div class="field has-addons">
-                <div className="control">
-                    <input className="input" type="text" placeholder="Find a Movie"></input>
+import React, { Component, useState } from 'react'
+
+
+export class searchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange = (event) => {
+        this.setState({
+            inputMovie: event.target.value
+        })
+    }
+    _handleSubmit = (event) => {
+        event.preventDefault();
+        alert(this.state.inputMovie);
+        //this.props.onSearch(this.state.inputMovie)
+    }
+
+    render(){
+        return(
+            <form onSubmit={this._handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Find a Movie" onChange={this._handleChange}></input>
+                    </div>
+                    <div className="control">
+                        <button className="button is-info">
+                        Search
+                        </button>
+                    </div>
                 </div>
-                <div class="control">
-                    <button className="button is-info">
-                    Search
-                    </button>
-                </div>
-            </div>
-        
-    );
+            </form>
+        )
+    }
 }
  
 export default searchForm;
